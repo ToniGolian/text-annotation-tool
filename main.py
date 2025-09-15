632395cdeeedeab10e562bfd2696671bcb3a96e9
Toni Golian <tonigolian@live.de>
2025-09-15 20:50:32 +0200

    module docstring

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+"""
+text-annotation-tool
+
+A lightweight text annotation tool for annotating spans of text with custom tags and attributes.
+
+Author: Toni Golian
+License: MIT
+"""
 from utils.app_builder import AppBuilder
 
 
